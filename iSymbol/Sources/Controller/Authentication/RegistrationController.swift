baeeec7443ea0ac2495eceef3e4354f6cfbe09ad
BEOMSU SHIM <rafa71802937@gmail.com>
2024-09-02 06:06:55 +0900

    feat: 회원가입 유효성 검사 적용

--- iSymbol/Sources/Controller/Authentication/RegistrationController.swift
+++ iSymbol/Sources/Controller/Authentication/RegistrationController.swift
@@ -22,4 +22,58 @@ final class RegistrationController: BaseViewController{
             .foregroundColor: UIColor.systemYellow
         ]
     }
+    // MARK: - Actions
+
+    @objc private func textFieldsDidChange() {
+        let username = registrationView.usernameTextField.text ?? ""
+        let email = registrationView.emailTextField.text ?? ""
+        let password = registrationView.pwTextField.text ?? ""
+        let confirmPassword = registrationView.confirmPWTextField.text ?? ""
+        let isUsernameValid = username.isValidUsername()
+        let isEmailValid = email.isValidEmail()
+        let isPasswordValid = password.isValidPassword()
+        let isConfirmPasswordValid = confirmPassword.isValidConfirmPassword(password: password)
+
+        if isUsernameValid && isEmailValid && isPasswordValid && isConfirmPasswordValid {
+            registrationView.updateSignUpButton(isEnabled: true)
+        } else {
+            registrationView.updateSignUpButton(isEnabled: false)
+        }
+    }
+
+    @objc private func handleSignUp() {
+        print("DEBUG: Sign up button tapped..")
+    }
+
+    override func setButtonActions() {
+        registrationView.usernameTextField.addTarget(
+            self,
+            action: #selector(textFieldsDidChange),
+            for: .editingChanged
+        )
+
+        registrationView.emailTextField.addTarget(
+            self,
+            action: #selector(textFieldsDidChange),
+            for: .editingChanged
+        )
+
+        registrationView.pwTextField.addTarget(
+            self,
+            action: #selector(textFieldsDidChange),
+            for: .editingChanged
+        )
+
+        registrationView.confirmPWTextField.addTarget(
+            self,
+            action: #selector(textFieldsDidChange),
+            for: .editingChanged
+        )
+
+        registrationView.signUpButton.addTarget(
+            self,
+            action: #selector(handleSignUp),
+            for: .touchUpInside
+        )
+    }
 }
